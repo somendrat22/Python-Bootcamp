d23807426afab2d919ff4d9a1e4a343c77f6fa07
macitasarnoh <94096121+macitasarnoh@users.noreply.github.com>
2022-10-12 23:41:39 -0400

    handerrank if stament

---
+++ HackerRank Problems/ifstament.py
@@ -0,0 +1,32 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+# #
+# If n is odd, print Weird
+# If n is even and in the inclusive range 2 of 5 to , print Not Weird
+# If n is even and in the inclusive range of 6 to 20 , print Weird
+# If n is even and greater than 20 , print Not Weird
+# Input Format
+
+# A single line containing a positive integer, .
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if n%2 == 0:
+    if n>=2 and n<=5:
+        print("Not Weird")
+    elif n>=6 and n<=20:
+        print("Weird")
+    elif n>20:
+        print("Not Weird")
+else:
+    print("Weird")   
