51064b056ceb08545dad89f20563c57fb34e3000
somendra tiwari <somendrat22@outlook.com>
2022-10-22 08:49:22 +0530

    Working on board

---
+++ TicTacToe/Board/Board.py
@@ -0,0 +1,14 @@
+
+class Board:
+
+    def __init__(self): 
+        self.board = [['_','_','_'], ['_','_','_'],['_', '_', '_']]
+    
+    def printBoard(self):
+        length = len(self.board[0])
+
+        for i in range(0, length):
+            for j in range(0, length):
+                print
+
+
